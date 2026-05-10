031466fe1936d723ea88fc3a9442e79ce2f0a0ae
PK <krzysztofpielot@gmail.com>
2026-05-10 15:10:15 +0200

    Improving the bucket sort algorithm

--- algorithms/bucketsort/parallel.py
+++ algorithms/bucketsort/parallel.py
@@ -1,62 +1,22 @@
 import multiprocessing as mp
 
-from .sequential import insertion_sort
-# from algorithms.mergesort.sequential import merge_sort
+from .utils import insertion_sort, distribute_to_buckets
 
-# def insertion_sort(arr):
-#     for i in range(1, len(arr)):
-#         key = arr[i]
-#         j = i - 1
-#         while j >= 0 and arr[j] > key:
-#             arr[j + 1] = arr[j]
-#             j -= 1
-#         arr[j + 1] = key
-#
-#     return arr
-
-# def bucket_sort(arr, num_buckets=None):
-#     if len(arr) == 0:
-#         return arr
-#
-#     if num_buckets is None:
-#         num_buckets = int(math.sqrt(len(arr)))
-#
-#     min_val, max_val = min(arr), max(arr)
-#     bucket_range = (max_val - min_val + 1) / num_buckets
-#     buckets = [[] for _ in range(num_buckets)]
-#
-#
-#     for num in arr:
-#         index = min(num_buckets - 1, int((num - min_val) / bucket_range))
-#         buckets[index].append(num)
-#
-#
-#     sorted_arr = []
-#     for bucket in buckets:
-#         sorted_bucket = merge_sort(bucket)
-#         sorted_arr.extend(sorted_bucket)
-#
-#     return sorted_arr
 
 def parallel_bucket_sort_worker(bucket, output_queue):
     sorted_bucket = insertion_sort(bucket)
-    # sorted_bucket = merge_sort(bucket)
     output_queue.put(sorted_bucket)
 
 def parallel_bucket_sort(arr, num_buckets):
     if len(arr) == 0:
         return arr
 
-    min_val, max_val = min(arr), max(arr)
-    bucket_range = (max_val - min_val + 1) / num_buckets
-    buckets = [[] for _ in range(num_buckets)]
-
-    for num in arr:
-        index = min(num_buckets - 1, int((num - min_val) / bucket_range))
-        buckets[index].append(num)
+    buckets = distribute_to_buckets(arr, num_buckets)
 
     processes = []
     queues = []
+
+    # Creating process
     for bucket in buckets:
         q = mp.Queue()
         p = mp.Process(target=parallel_bucket_sort_worker, args=(bucket, q))
@@ -65,11 +25,14 @@ def parallel_bucket_sort(arr, num_buckets):
         p.start()
 
     sorted_buckets = []
+
     for p, q in zip(processes, queues):
         sorted_buckets.append(q.get())
         p.join()
 
+    # Merging
     sorted_arr = []
+
     for bucket in sorted_buckets:
         sorted_arr.extend(bucket)
 
--- algorithms/bucketsort/sequential.py
+++ algorithms/bucketsort/sequential.py
@@ -1,25 +1,19 @@
 import math
 
-from .utils import insertion_sort
+from .utils import insertion_sort, distribute_to_buckets
 
 def bucket_sort(arr, num_buckets=None):
     if len(arr) == 0:
         return arr
 
+    # Heuristics
     if num_buckets is None:
         num_buckets = int(math.sqrt(len(arr)))
 
-    min_val, max_val = min(arr), max(arr)
-    bucket_range = (max_val - min_val + 1) / num_buckets
-    buckets = [[] for _ in range(num_buckets)]
-
-    for num in arr:
-        index = min(num_buckets - 1, int((num - min_val) / bucket_range))
-        buckets[index].append(num)
-
+    buckets = distribute_to_buckets(arr, num_buckets)
     sorted_arr = []
+
     for bucket in buckets:
-        # sorted_bucket = merge_sort(bucket)
         sorted_bucket = insertion_sort(bucket)
         sorted_arr.extend(sorted_bucket)
 
