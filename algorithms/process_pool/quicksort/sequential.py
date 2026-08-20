69a9dcf535b6ffbd5b78dd242a6b4a2103cf6398
PK <krzysztofpielot@gmail.com>
2026-08-20 19:47:01 +0200

    Quicksort at ProcessPoolExecutor, utils and sequential

---
+++ algorithms/process_pool/quicksort/sequential.py
@@ -0,0 +1,14 @@
+from algorithms.process_pool.quicksort.utils import partition
+
+
+def quicksort(arr, low=0, high=None):
+    if high is None:
+        high = len(arr) - 1
+
+    if low >= high:
+        return
+
+    index = partition(arr, low, high)
+
+    quicksort(arr, low, index - 1)
+    quicksort(arr, index, high)
---
+++ algorithms/process_pool/quicksort/utils.py
@@ -0,0 +1,90 @@
+import ctypes
+from multiprocessing import shared_memory
+
+
+# {cores: próg_min_size}
+PARALLEL_CUTOFF = {
+    2: 50_000,
+    4: 100_000,
+    8: 300_000,
+    16: 600_000,
+    32: 1_200_000,
+    # 64: 2_400_000,
+    # 128: 4_800_000,
+}
+
+
+def get_parallel_cutoff(cores):
+    return PARALLEL_CUTOFF.get(cores)
+
+
+def partition(arr, low, high):
+    pivot = arr[(low + high) // 2]
+
+    i = low
+    j = high
+
+    while i <= j:
+        while arr[i] < pivot:
+            i += 1
+        while arr[j] > pivot:
+            j -= 1
+        if i <= j:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+            j -= 1
+
+    return i
+
+
+def get_ctype(dtype):
+    if dtype is int:
+        return ctypes.c_longlong
+    elif dtype is float:
+        return ctypes.c_double
+
+    raise ValueError("dtype musi być int lub float")
+
+
+def create_shared_array(data, dtype):
+    c_type = get_ctype(dtype)
+
+    shm = shared_memory.SharedMemory(
+        create=True,
+        size=len(data) * ctypes.sizeof(c_type)
+    )
+
+    shared_array = (c_type * len(data)).from_buffer(shm.buf)
+    shared_array[:] = data
+
+    return shm, shared_array
+
+
+def attach_shared_array(name, length, dtype):
+    c_type = get_ctype(dtype)
+
+    shm = shared_memory.SharedMemory(name=name)
+    shared_array = (c_type * length).from_buffer(shm.buf)
+
+    return shm, shared_array
+
+
+def calculate_min_size(data_size, max_depth):
+    cores = 1 << max_depth
+
+    cutoff = get_parallel_cutoff(cores)
+    fallback = max(5000, data_size // (cores * 8))
+
+    if cutoff is None:
+        return fallback
+
+    return cutoff
+
+
+def close_shared_memory(shm):
+    shm.close()
+
+
+def destroy_shared_memory(shm):
+    shm.close()
+    shm.unlink()
